961ba1c95769829dd0403699370ce656db555c46
Ty Klabacka <ty-klabacka@pluralsight.com>
2024-06-21 11:18:55 -0600

    move line(s) up and down

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -9,4 +9,15 @@ map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 
+-- Move selected block of text up with Ctrl + k
+map("v", "<C-k>", ":m '<-2<CR>gv=gv", { desc = "Move selected block up" })
+
+-- Move selected block of text down with Ctrl + j
+map("v", "<C-j>", ":m '>+1<CR>gv=gv", { desc = "Move selected block down" })
+
+-- Move line up with Ctrl + kk
+map("n", "<C-k><C-k>", ":m .-2<CR>==", { desc = "Move line up" })
+
+-- Move line down with Ctrl + jj
+map("n", "<C-j><C-j>", ":m .+1<CR>==", { desc = "Move line down" })
 map("t", "<Esc>", "<C-\\><C-n>")
