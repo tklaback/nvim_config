27e953daead865a5a81856a1101eab0fce718c14
Ty Klabacka <klabacka.ty@gmail.com>
2024-06-25 07:20:19 -0600

    configured toggleterm

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -39,30 +39,8 @@ return {
   {
     'akinsho/toggleterm.nvim',
     version = "*",
-    config = function ()
-    require("toggleterm").setup {
-      size = 120,
-      open_mapping = [[<leader>tt]],
-      hide_numbers = true,
-      shade_filetypes = {},
-      shade_terminals = true,
-      shading_factor = 2,
-      start_in_insert = true,
-      insert_mappings = true,
-      persist_size = true,
-      direction = "float",
-      close_on_exit = false,
-      shell = vim.o.shell,
-      float_opts = {
-          border = "curved",
-          winblend = 0,
-          highlights = {
-              border = "Normal",
-              background = "Normal",
-          },
-      },
-    }
-    end,
-    lazy = false
+    config = function()
+      require"configs.toggleterm"
+    end
   }
 }
