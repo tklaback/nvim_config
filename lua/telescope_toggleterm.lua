291ba7f3b4b391ed91612ac9ca6ec342381de1aa
Ty Klabacka <klabacka.ty@gmail.com>
2024-06-27 22:56:19 -0600

    now able to create terminal

--- lua/telescope_toggleterm.lua
+++ lua/telescope_toggleterm.lua
@@ -3,9 +3,44 @@ local actions_state = require "telescope.actions.state"
 local pickers = require "telescope.pickers"
 local finders = require "telescope.finders"
 local conf = require("telescope.config").values
-local toggleterm = require "toggleterm.terminal"
+local Terminal = require("toggleterm.terminal").Terminal
 
 local M = {}
+
+local function exit_terminal(prompt_bufnr)
+   local selection = actions_state.get_selected_entry()
+   if selection == nil then
+      return
+   end
+   local bufnr = selection.value.bufnr
+   local current_picker = actions_state.get_current_picker(prompt_bufnr)
+   current_picker:delete_selection(function(selection)
+      vim.api.nvim_buf_delete(bufnr, { force = true })
+   end)
+end
+
+local function create_term(prompt_bufnr)
+
+
+	local function on_open_terminal()
+		actions.close(prompt_bufnr)
+
+		-- the autocommand setup in telescope/init.lua that starts insert mode on leaving the telescope buffer
+		-- won't work here since the cursor may move to a non-toggleterm buftype for a brief moment while the
+		-- toggleterm buffer is being created
+		-- utils.start_insert_mode()
+	end
+
+	local term = Terminal:new({ hidden = true })
+
+  term:open()
+end
+
+local telescope_mappings = {
+    ['<C-c>'] = exit_terminal,
+    ['<C-i>'] = create_term
+}
+
 
 function M.terminal_picker()
   local bufnrs = vim.tbl_filter(function(bufnr)
@@ -35,8 +70,6 @@ function M.terminal_picker()
     table.insert(buffers, element)
   end
 
-  print "Here 2"
-  vim.api.nvim_out_write(vim.inspect(buffers))
   pickers
     .new({}, {
       prompt_title = "Terminal Buffers",
@@ -61,16 +94,16 @@ function M.terminal_picker()
           local bufnr = tostring(selection.value.bufnr)
           local toggle_number = selection.value.toggle_number
           require("toggleterm").toggle_command(bufnr, toggle_number)
-          vim.defer_fn(function()
-            vim.cmd "stopinsert"
-          end, 0)
+          -- vim.defer_fn(function()
+          --   vim.cmd "stopinsert"
+          -- end, 0)
         end)
-        -- local mappings = require("config").options.telescope_mappings
-        -- for keybind, action in pairs(mappings) do
-        --   map("i", keybind, function()
-        --     action(prompt_bufnr)
-        --   end)
-        -- end
+        for keybind, action in pairs(telescope_mappings) do
+          print(keybind)
+          map("n", keybind, function()
+            action(prompt_bufnr)
+          end)
+        end
         return true
       end,
     })
