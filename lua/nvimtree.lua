f1190d59b4cd68e06a7b416557990448de8f7228
Ty Klabacka <ty-klabacka@pluralsight.com>
2024-06-28 11:16:47 -0600

    added command to focus on buffer window

--- lua/nvimtree.lua
+++ lua/nvimtree.lua
@@ -1,7 +1,26 @@
 local nvim_tree = require('nvim-tree')
 
+local function my_on_attach(bufnr)
+  local api = require "nvim-tree.api"
+
+  local function opts(desc)
+    return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  -- default mappings
+  api.config.mappings.default_on_attach(bufnr)
+
+  -- custom mappings
+  -- vim.keymap.set('n', '<C-t>', api.tree.change_root_to_parent,        opts('Up'))
+  -- vim.keymap.set('n', '?',     api.tree.toggle_help,                  opts('Help'))
+  vim.keymap.set('n', '<leader>a', function()
+    vim.cmd('wincmd l')
+  end, opts('Focus Buffer'))
+end
+
 nvim_tree.setup({
   filters = {
     dotfiles = false,  -- Set this to false to show hidden files
   },
+  on_attach = my_on_attach,
 })
