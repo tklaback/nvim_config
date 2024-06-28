6f931bed6f56676c41088b48ecaa55e584b9e5b6
Ty Klabacka <ty-klabacka@pluralsight.com>
2024-06-28 08:57:51 -0600

    added better mappings to toggle term manager

--- lua/configs/toggleterm_manager.lua
+++ lua/configs/toggleterm_manager.lua
@@ -7,6 +7,9 @@ toggleterm_manager.setup {
 	    n = {
 	      ["<CR>"] = { action = actions.toggle_term, exit_on_action = true },
 	      ["x"] = { action = actions.delete_term, exit_on_action = false },
+        ["<C-i>"] = { action = actions.create_term, exit_on_action = false }, -- creates a new terminal buffer
+        ["<C-d>"] = { action = actions.delete_term, exit_on_action = false }, -- deletes a terminal buffer
+        ["<C-r>"] = { action = actions.rename_term, exit_on_action = false },
 	    },
 	},
 }
