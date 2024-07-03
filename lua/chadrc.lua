d5a719ba1805b89f6fe60e518ac34de859c568ec
Ty Klabacka <ty-klabacka@pluralsight.com>
2024-07-03 15:38:28 -0600

    now buffer reloads on change

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,17 +1,27 @@
--- This file  needs to have same structure as nvconfig.lua 
+-- This file  needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/NvChad/blob/v2.5/lua/nvconfig.lua
 
 ---@type ChadrcConfig
 local M = {}
 
 M.ui = {
-	theme = "nightowl",
+  theme = "nightowl",
 
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+  -- hl_override = {
+  -- 	Comment = { italic = true },
+  -- 	["@comment"] = { italic = true },
+  -- },
 }
 
-require"nvimtree"
+-- Reload the buffer when the file changes outside of Neovim
+vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "CursorHold", "CursorHoldI" }, {
+  pattern = "*",
+  callback = function()
+    if vim.fn.getcmdwintype() == "" then
+      vim.cmd "checktime"
+    end
+  end,
+})
+
+require "nvimtree"
 return M
